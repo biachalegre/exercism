edba09e37710914448f88dd4fb9fccd93f006b20
Beatriz Chalegre <anabeachalegre@gmail.com>
2023-04-04 21:04:59 -0300

    Solving first exercise: Hello World

---
+++ hello-world/src/Greeter.java
@@ -0,0 +1,11 @@
+public class Greeter {
+
+    public String getGreeting() {
+        return "Hello, World!";
+    }
+
+    public static void main(String[] args) {
+        Greeter greeter = new Greeter();
+        System.out.println(greeter.getGreeting());
+    }
+}
